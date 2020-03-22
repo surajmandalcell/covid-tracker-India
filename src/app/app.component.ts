08f73441a5ea6315fe1c9e4b30d8974ed9ad5455
Suraj Mandal <surajmandalcell@gmail.com>
2020-03-22 23:06:53 +0530

    working overall stats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +28,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  //India Specific Stats
+  deathsIn:string;
+  confirmedIn:string;
+  recoveredIn: string;
+  newDeathsTodayIn:string;
+  newConfirmedTodayIn:string;
+  // Global Stats
+  newCases:string;
+  totalDead:string;
+  newDeaths:string;
+  totalCases:string;
+  totalRecovered:string;
+  // Api url
+  readonly apiUrl = 'https://thevirustracker.com';
+
+  constructor(public httpClient: HttpClient){
+    this.getCountryData();
+    this.getGlobalData();
+  }
+
+  getCountryData(){
+    this.httpClient.get(this.apiUrl+'/free-api?countryTotal=IN').subscribe((res:any) => {
+      console.log(res);
+      this.confirmedIn = res.countrydata[0].total_cases;
+      this.deathsIn = res.countrydata[0].total_deaths;
+      this.recoveredIn = res.countrydata[0].total_recovered;
+      this.newDeathsTodayIn = res.countrydata[0].total_new_deaths_today;
+      this.newConfirmedTodayIn = res.countrydata[0].total_new_cases_today;
+    });
+  }
+
+  getGlobalData(){
+    this.httpClient.get(this.apiUrl+'/free-api?global=stats').subscribe((res:any) => {
+      console.log(res);
+      this.totalCases = res.results[0].total_cases;
+      this.totalDead = res.results[0].total_deaths;
+      this.totalRecovered = res.results[0].total_recovered;
+      this.newDeaths = res.results[0].total_new_deaths_today;
+      this.newCases = res.results[0].total_new_cases_today;
+    });
+  }
+
   title = 'covid-tracker-India';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     MatCardModule,
+    MatTableModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
